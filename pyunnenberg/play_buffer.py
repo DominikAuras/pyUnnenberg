a1fa5aa99d08d6b17f14c11a38b9f154c048043c
Dominik Auras <DominikAuras@users.noreply.github.com>
2014-07-20 19:21:04 +0200

    renaming,code cleanup in play_buffer.py

--- pyunnenberg/play_buffer.py
+++ pyunnenberg/play_buffer.py
@@ -10,25 +10,32 @@ def _chunks(n, iterable, padvalue=None):
     return izip_longest(*[iter(iterable)]*n, fillvalue=padvalue)
   
 class PlayFloatBuffer:
-  CHUNK = 1024
+  CHUNK = 32
   FORMAT = alsaaudio.PCM_FORMAT_FLOAT_LE
   CARD = "default"
 
-  def __init__(self, buffer, framerate):
+  def __init__(self, buffer, framerate, chunk = CHUNK):
     """ Init audio stream """
-    self.buffer = map(lambda x: array('f',x).tostring(), _chunks(self.CHUNK,buffer,0.))
-    self.p = alsaaudio.PCM(card=self.CARD)
-    self.p.setchannels(1)
-    self.p.setrate(framerate)
-    self.p.setformat(self.FORMAT)
-    self.p.setperiodsize(self.CHUNK)
-    self._stop = False
+    self.chunk = chunk
+    if (len(buffer) % self.chunk):
+      print("Warning: buffer length is not a multiple of {0}, padding with zeros".format(self.chunk))
+    self.buffer = map(lambda x: array('f',x).tostring(), _chunks(self.chunk,buffer,0.))
+    self.pcm = alsaaudio.PCM(card=self.CARD)
+    self.framerate = framerate
+    self._setup()
+    
+  def _setup(self):
+    self.pcm.setchannels(1)
+    self.pcm.setrate(self.framerate)
+    self.pcm.setformat(self.FORMAT)
+    self.pcm.setperiodsize(self.chunk)
 
   def play(self):
     """ Play entire buffer """
     for data in self.buffer:
-      while self.p.write(data) == 0:
+      while self.pcm.write(data) == 0:
         time.sleep(0)
+        
     
 if __name__=='__main__':
   from math import sin, pi
